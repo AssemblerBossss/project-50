cf0018b5a16406a67512b631e5ae9145b400bd68
DimoonNazarov <nazarovdima39@mail.ru>
2024-08-23 18:46:02 +0300

    Добавил функция load разных форматов

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,17 +1,16 @@
+import json
+import yaml
 
 
 def open_file(filename_1: str) -> tuple[str, str]:
     """
     Opens a file and returns its content and format.
 
-    Args:
-        filename_1 (str): Path to the file.
-
-    Returns:
-        tuple[str, str]: A tuple containing the content of the file and its format. The format can be either 'json' or 'yaml'.
-
-    Raises:
-        FileNotFoundError: If the specified file does not exist.
+    :param filename_1: Path to the file.
+    :return: A tuple containing the content of the file
+            and its format. The format can be either 'json' or 'yaml'.
+    :rtype: tuple[str, str]:
+    :raise: FileNotFoundError: If the specified file does not exist.
     """
 
     with open(filename_1, encoding='utf-8') as file:
@@ -21,3 +20,24 @@ def open_file(filename_1: str) -> tuple[str, str]:
         if filename_1.endswith('.yaml') or filename_1.endswith('yml'):
             data_format = 'yaml'
     return text, data_format
+
+
+def parse_data(data: str, data_format: str) -> dict:
+    """
+    Parsing data from JSON or YAML into a dictionary.
+
+    This function accepts data as a string and the name
+    of the format to which this data belongs.
+    Depending on the name of the format, JSON or YAML
+    data is converted into a dictionary. The data is returned.
+
+    :param data: Text data
+    :param data_format: Format
+    :return: Parsed data
+    :rtype: dict
+    """
+
+    if data_format == 'yaml':
+        return yaml.load(data, yaml.Loader)
+    if data_format == 'json':
+        return json.loads(data)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,11 @@
 from gendiff.cli import parse_arguments
+from gendiff.parser import parse_data, open_file
 
 
 def main():
     arguments = parse_arguments()
+    print(parse_data(*open_file(arguments.first_file)))
+    # print(arguments)
 
 
 if __name__ == '__main__':
